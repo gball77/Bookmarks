ec50ce65d82169b1c72b4cb41df17b43139aa39b
GBall77 <davisgamelle@gmail.com>
2015-01-20 12:03:28 +0000

    Initial Commit

---
+++ app/controllers/site_controller.rb
@@ -0,0 +1,8 @@
+class SiteController < ApplicationController
+  def index
+    
+    # retrieve all Bookmarks ordered by descending creation timestamp
+    @bookmarks = Bookmark.order('created_at desc')
+    
+  end
+end
